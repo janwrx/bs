b3e7abe604f14ab1cf8fc6f9701e19c7820cf6ad
janwrx <janzosa@gmail.com>
2018-10-19 16:59:03 -0700

    updatedbs

--- Desktop/battleship/src/App.js
+++ Desktop/battleship/src/App.js
@@ -5,12 +5,20 @@ import Board from './Board'
 import Squares from './Squares'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicks: 25,
+  };
+}
 
   render() {
     return (
       <div className="App">
       <h1>BATTLESHIP</h1>
-        <Board />
+      <h2>WHAT THE SHIP!?</h2>
+
+        <Board onClick={this.DecreaseItem}/>
       </div>
     );
   }
--- Desktop/battleship/src/Board.js
+++ Desktop/battleship/src/Board.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Squares from './Squares.js'
 
@@ -8,64 +7,83 @@ class Board extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        emptySquare: Array(5).fill(null),
+        emptySquare: Array(100).fill(null),
         gameOver: false,
-        movesMade: 0,
-        clickColor: ["blue", "red"]
+        clicked: false,
+        movesMade: 25,
+        targets: [2, 0, 44],
+        clicks: 25,
     }
+    this.placeShips()
   }
-
-  torpedoed(i) {
-    console.log("clicked")
-    let missedTarget = false
-    let hitTarget = true
-    if (this.state.emptySquare[i] === true) {
-      return (this.state.clickColor)
-
-    } else if (this.state.emptySquare[i] === false ) {
-      return (this.state.missedTarget)
+  DecreaseItem = () => {
+      this.setState({ clicks: this.state.clicks - 1 });
     }
 
-      return this.state.clickColor;
+  targetSquare(index) {
+    // console.log(this.state.emptySquare);
+    // console.log(this.state.emptySquare[index] === 1)
+    //make a copy of state
+    let { emptySquare } = this.state
+    if (this.state.emptySquare[index] === 1 || this.state.emptySquare[index] === "HIT") {
+      //register a hit
+      //assign new value to index in emptySquare
+      emptySquare[index] = "HIT"
+      //
+      // return this.torpedoed()
+    } else {
+       emptySquare[index] = "MISS"
+    }
+      //update state
+      this.setState({emptySquare: emptySquare})
+    }
+
+    //add ships into emptySquare
+    //replace null with a number
+    placeShips(){
+      //make a copy of emptySquare
+      let { targets, emptySquare } = this.state
+      //update emptySquare with ship positions
+      targets.forEach(function(el) {
+        emptySquare[el] = 1
+      })
+      //update state (emptySquare) with new ship positions
+      this.setState({
+        emptySquare: emptySquare
+      })
     }
 
 
   resetGame() {
-    var emptySquare = Array(25).fill(null)
-    this.setState({
-      Board: emptySquare,
-      gameOver: false,
-      movesMade: 0,
-    })
+      window.location.reload();
   }
 
   render() {
-    let emptySquare = this.state.emptySquare.map((element, index) => {
-      return (
-        <Squares id={index} colors={this.state.clickColor}
-        onClick={this.torpedoed.bind(this)}/>
-      )
-    })
-
     return (
       <div >
+      <h3>Torpedoes Remaining {this.state.clicks}</h3>
         <button className="reset" type="submit" onClick={() => this.resetGame()}>Reset</button>
-        <div className="Board">
-          <div>
-            {emptySquare}
-          </div>
-          <div>
-            {emptySquare}
-          </div>
-          <div>
-            {emptySquare}
-          </div>
-          <div>
-            {emptySquare}
-          </div>
-          <div>
-            {emptySquare}
-          </div>
+        <div className="Board" >
+          {this.state.emptySquare.map(
+            (element, index) => {
+            //if this square has a ship
+            if(element === 'HIT') {
+                //return a red square
+                return <div className="hitTarget" key={index} onClick={() => this.targetSquare(index)} >{index}</div>
+            } else if
+              (element === 'MISS') {
+                return <div className="missedTarget" key={index} onClick={() => this.targetSquare(index)} >{index}</div>
+              }
+              else {
+            //else
+                //return a white square
+              return <div className="eachSquare" style={{backgroundColor: this.state.clickColor}}  key={index} onClick={() => this.targetSquare(index)}>
+                {index}
+              </div>
+            }
+            })
+            }
+
         </div>
       </div>
     );
